2792b9b5e0c61891ea8257553fb9bd74e99bc09f
JustineELIAS <j.elias@envisol.fr>
2024-06-04 19:05:04 +0200

    first commit

---
+++ vaster_front/nuxt.config.ts
@@ -0,0 +1,32 @@
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+export default defineNuxtConfig({
+  //...
+  build: {
+    transpile: ['vuetify'],
+  },
+  modules: [
+    (_options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', (config) => {
+        // @ts-expect-error
+        config.plugins.push(vuetify({ autoImport: true }))
+      })
+    },
+    //...
+  ],
+  vite: {
+    vue: {
+      customElement: true,
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
+  runtimeConfig: {
+      // The private keys which are only available server-side
+      apiSecret: '123',
+      // Keys within public are also exposed client-side
+      public: {
+        apiBase: '/api'
+      }
+    }
+})
---
+++ vaster_front/pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+    <NuxtLayout>
+      <v-app>
+        <div>
+            <VFileInput v-model="files" label="Selectionner fichier" ></VFileInput>
+        </div>
+        <div>
+            Hello World!
+            {{ files.values }}
+        </div>
+        <v-btn color="success" @click="goFunc(files)">Go!</v-btn>
+      </v-app>
+    </NuxtLayout>
+</template>
+
+<script setup lang="ts">
+const runtimeConfig = useRuntimeConfig()
+console.log(runtimeConfig.apiSecret)
+const files = ref([File])
+var filename = ""
+// watch(file, )
+function goFunc(files:any){
+    console.log("file");
+    console.log("file", files.value)
+}
+
+</script>
